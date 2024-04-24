2f24cdae5f910ec6822a773d00b3c7251e8a37ba
Avishek Sen <x0rzavi@gmail.com>
2024-04-24 22:28:01 +0530

    Add POC for past events page

--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.events, name="events"),
     path("add/", views.add_event, name="add_event"),
+    path("past/", views.past_events, name="past_events"),
     path("event/<int:event_id>/", views.view_event, name="view_event"),
 ]
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from .models import Event
 
 
 # Create your views here.
 def events(request):
-    events = Event.objects.all()
+    # events = Event.objects.all()
+    events = Event.objects.filter(date__gte=timezone.now())
     context = {"events": events}
     return render(request, "events/events.html", context)
+
+
+def past_events(request):
+    events = Event.objects.filter(date__lt=timezone.now())
+    context = {"events": events}
+    return render(request, "events/past_events.html", context)
 
 
 def add_event(request):
