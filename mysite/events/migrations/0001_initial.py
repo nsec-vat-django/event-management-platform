3c2d5fa349be8b23b0e12b938f6de6cedbb273f4
Avishek Sen <x0rzavi@gmail.com>
2024-04-24 15:54:19 +0530

    Reformat files with Ruff

--- mysite/events/migrations/0001_initial.py
+++ mysite/events/migrations/0001_initial.py
@@ -4,21 +4,27 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Event',
+            name="Event",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField()),
-                ('date', models.DateTimeField()),
-                ('location', models.CharField(max_length=200)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=200)),
+                ("description", models.TextField()),
+                ("date", models.DateTimeField()),
+                ("location", models.CharField(max_length=200)),
             ],
         ),
     ]
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Event
 
+
 # Create your views here.
 def index(request):
     events = Event.objects.all()
