834a978468e8d28374f10809c585dbeb6de7eb5a
Avishek Sen <x0rzavi@gmail.com>
2024-04-24 15:01:44 +0530

    Add models for events

--- mysite/events/models.py
+++ mysite/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
