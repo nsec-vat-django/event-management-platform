59cd6c3a96bf555e80c4c8761e8881569a3ab859
Avishek Sen <x0rzavi@gmail.com>
2024-04-24 20:54:37 +0530

    Add views for adding events

--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add/", views.add_event, name="add_event"),
 ]
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -7,3 +7,8 @@ def index(request):
     events = Event.objects.all()
     context = {"events": events}
     return render(request, "events/events.html", context)
+
+
+def add_event(request):
+    return render(request, "events/add_events.html")
+
