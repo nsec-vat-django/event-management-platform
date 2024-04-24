c604675ea936ce5f3d537b3c0eb5e385d5351b08
Avishek Sen <x0rzavi@gmail.com>
2024-04-24 15:36:57 +0530

    Remove http404 error on empty event list

--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,12 +1,8 @@
-from django.http import Http404
 from django.shortcuts import render
 from .models import Event
 
 # Create your views here.
 def index(request):
-    try:
-        events = Event.objects.all()
-        context = {"events": events}
-        return render(request, "events/index.html", context)
-    except Event.DoesNotExist:
-        raise Http404("No events found")
+    events = Event.objects.all()
+    context = {"events": events}
+    return render(request, "events/index.html", context)
