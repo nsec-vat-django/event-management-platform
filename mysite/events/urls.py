b1eff052caceba5efbb2429514abeb24fafe6474
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-04-26 11:11:15 +0530

    Add create_event view for adding events

--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("add/", views.add_events, name="add_events"),
     path("past/", views.past_events, name="past_events"),
     path("view/<int:event_id>/", views.view_events, name="view_events"),
+    path("create-event/",views.create_event),
 ]
--- mysite/events/views.py
+++ mysite/events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from events.models import Event
 
@@ -24,3 +24,24 @@ def add_events(request):
 def view_events(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     return render(request, "events/view_events.html", {"event": event})
+
+def create_event(request):
+    if request.method == "POST":
+        #fetch data
+        emp_name = request.POST.get('title')
+        emp_description = request.POST.get('description')
+        emp_date = request.POST.get('date')
+        emp_location = request.POST.get('location')
+
+        #create model object and set the data
+        e= Event()
+        e.title = emp_name
+        e.description = emp_description
+        e.date = emp_date
+        e.location = emp_location
+
+        #saving the data in database
+        e.save()
+
+        return redirect("/events/")
+    return render(request, "events/add_events.html",{})
