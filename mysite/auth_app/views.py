903b073ee7f11ab73996366835ef7fb2a0155461
Shruti Sen <senshruti2020@gmail.com>
2024-04-26 21:36:52 +0530

    added my events page

--- mysite/auth_app/views.py
+++ mysite/auth_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
-# Create your views here.
 
 
 def register_view(request):
--- mysite/events/urls.py
+++ mysite/events/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("past/", views.past_events, name="past_events"),
     path("view/<int:event_id>/", views.view_events, name="view_events"),
     path("cancel/<int:event_id>/", views.cancel_event, name="cancel_events"),
+    path('my_events/', views.my_events, name='my_events'),
 ]
