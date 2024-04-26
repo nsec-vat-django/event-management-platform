6750024cf85cb059dea9187b19d728b22455d6c5
Avishek Sen <x0rzavi@gmail.com>
2024-04-26 18:40:42 +0530

    Reformat files

--- mysite/auth_app/urls.py
+++ mysite/auth_app/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('register/',views.register_view,name='register'),
-    path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout')
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "auth_app"
+    "auth_app",
 ]
 
 AUTHENTICATION_BACKENDS = [
