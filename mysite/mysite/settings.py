8dfc525a0a61d60414cbc02e43c21ff468838f75
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-04-27 06:49:40 +0530

    Made it responsive and improved styling

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6w_o*1l6d=acfwtf@v3(kz^07zf8%&p804gix_qv8@ze2$@7!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104','localhost','127.0.0.1']
 
 
 # Application definition
